6654397311ae33890bf6654666d48fbe8e37905d
lihao <591353643@qq.com>
2018-11-26 16:50:49 +0800

    1.修改babel配置;

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
 require("babel-core/register");
-require("babel-core").transform("code", {
-    plugins: ["transform-runtime"]
-});
 exports.BinaryTree = require("./lib/BinaryTree").default;
--- test/index.text.js
+++ test/index.text.js
@@ -1,5 +1,5 @@
 const { BinaryTree } = require("../index.js");
 
-const treeEntity = new BinaryTree({});
+const treeEntity = new BinaryTree([5,3,8,9,12,6,3,22,12,11]);
 
 console.log(treeEntity);
